def38b49520c772dd81f7b04b3e3d3fd92ddf8c6
Sourish <basu.sourish58@gmail.com>
2025-02-20 11:14:03 +0530

    main.tf change

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "terraformsasourishbasu"
+    container_name      = "tfstate"
+    key                 = "terraform.tfstate"
+  }
 }
 
 
